081ff8bb517fd994d6cdde52aee011c64ad4f8a0
Keith Fung <fungsenkeith@gmail.com>
2020-12-14 19:31:22 -0500

    adding components

--- google-book-search/client/src/App.js
+++ google-book-search/client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Banner />
+      <Route exact path={["/", "/search"]} component={Search} />
+      <Route exact path="/saved" component={Saved} />
+    </Router>
   );
 }
 
