e7e9dec34ca861ebd6aeb41c4d1aa78bebfd9d8e
Keith Fung <fungsenkeith@gmail.com>
2020-12-14 19:25:52 -0500

    book model

---
+++ google-book-search/models/Book.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Schema for the model
+const BookSchema = new Schema({
+  title: { type: String, unique: true },
+  authors: Array,
+  description: String,
+  image: String,
+  link: String,
+});
+
+// Defining a model using the schema
+const Book = mongoose.model("Book", BookSchema);
+
+// Exporting the model
+module.exports = Book;
